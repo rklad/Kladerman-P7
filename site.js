338026200d3d5f2d47489095e69ecae508ae81ab
rklad <525RKladerman@frhsd.com>
2024-12-09 14:44:43 -0500

    added variables for title, owner, and github

--- site.js
+++ site.js
@@ -34,6 +34,9 @@ const vue_app = Vue.createApp({
             // This holds your movies.json data.
             movies: [],
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+            title: "Randy's Top 8 Movies", 
+            owner: "Randy Kladerman",
+            github: "https://github.com/rklad/Kladerman-P7"
          
       }
     },
